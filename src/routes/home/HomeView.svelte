1acbb38704720fb383b7043aee2e5634629ab5f2
HugeErick <erick.parada101@gmail.com>
2026-08-06 21:39:54 -0600

    feat: logout functionality

--- src/routes/home/HomeView.svelte
+++ src/routes/home/HomeView.svelte
@@ -1,7 +1,22 @@
+<script lang="ts">
+  // Homeview.svelte
+  import { Button } from "$lib/components/ui/button/index.js";
+
+</script>
 
 <div class="border-2 border-red-500 w-full flex flex-col justify-center align-middle">
   <div>
     <div class="my-20">
+      <div class="flex gap-4 align-middle justify-center">
+        <form method="POST" action="/home?/logout">
+          <Button type="submit" variant="outline">
+            <span>
+              Log out
+            </span>
+          </Button>
+        </form>
+
+      </div>
       <h1>
         Home Nur 
       </h1>
